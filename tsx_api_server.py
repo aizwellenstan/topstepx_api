286d293b1c18c14e7ee677d169bab43704ee0dd3
aizwellenstan <aizwellenstan@gmail.com>
2025-10-30 22:56:11 +0900

    update entry

--- tsx_api_server.py
+++ tsx_api_server.py
@@ -359,7 +359,7 @@ async def place_oco_generic(data, entry_type):
     tp = round_to_tick(tp, tick_size)
     sl = round_to_tick(sl, tick_size)
 
-    if op > sl: op += tick_size * 5
+    if op > sl: op += tick_size * 6
     else: op -= tick_size
 
     sl_ticks = abs(op - sl) / tick_size
@@ -385,8 +385,8 @@ async def place_oco_generic(data, entry_type):
         tp = round_to_tick(tp, tick_size)
         sl = round_to_tick(sl, tick_size)
 
-    if quantity > 3:
-        quantity = 3
+    # if quantity > 3:
+    #     quantity = 3
 
     # if quantity <= 0:
     #     return jsonify({"error": "Calculated quantity is zero"}), 400
