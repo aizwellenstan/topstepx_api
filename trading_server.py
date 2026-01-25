3288b6a83fd9c47bb80191ca7210a1514d4ee4e2
aizwellenstan <aizwellenstan@gmail.com>
2026-01-26 00:11:23 +0900

    lts

--- trading_server.py
+++ trading_server.py
@@ -325,8 +325,11 @@ class TradingServer:
                 raise HTTPException(400, "SL too close to OP")
 
             risk_budget = (balance - maximum_loss) * self.risk_pct
-            if self.risk_pct > 0.049 and custom_tag == "AllTimeLongES":
-                risk_budget = (balance - maximum_loss) - 64
+            if self.risk_pct > 0.055:
+                if custom_tag in [
+                    "AllTimeLongES"
+                ]:
+                    risk_budget = (balance - maximum_loss) - 97
 
             dynamic_contracts = math.floor(risk_budget / (sl_ticks * tick_value))
             quantity = max(dynamic_contracts, 1)
